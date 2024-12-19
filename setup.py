443b59619731a83be1b21f79891511ee181a0e19
cqian23 <168020313+cqian23@users.noreply.github.com>
2024-12-19 15:19:10 -0800

    Update version

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="spanner-graph-notebook",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(),
     install_requires=[
         "networkx", "numpy", "google-cloud-spanner", "ipython",
