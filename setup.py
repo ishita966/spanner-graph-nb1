169cd30a143a78121900061385a958ce172b9519
Henry Ventura <hvent90@gmail.com>
2025-01-22 14:29:30 -0800

    chore: Bump version to v1.0.8

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="spanner-graph-notebook",
-    version="v1.0.7",
+    version="v1.0.8",
     packages=find_packages(),
     install_requires=[
         "networkx", "numpy", "google-cloud-spanner", "ipython",
