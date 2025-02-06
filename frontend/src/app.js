26a60e09101a0354b385e97c97b31633095c134c
Eric Feiveson <ericfe+github@google.com>
2025-02-06 15:48:55 -0800

    Fix visualization after merge

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -71,7 +71,7 @@ class SpannerApp {
         table: null
     };
 
-    constructor({id, port, project, instance, database, mock, mount, query}) {
+    constructor({id, port, params, mount, query}) {
         this.id = id;
         this.lastQuery = query;
 
@@ -83,7 +83,7 @@ class SpannerApp {
 
         this.scaffold();
 
-        this.server = new GraphServer(port, project, instance, database, mock);
+        this.server = new GraphServer(port, params);
         this.server.query(query)
             .then(data => {
                 if (!data) {
--- spanner_graphs/magics.py
+++ spanner_graphs/magics.py
@@ -62,69 +62,6 @@ def _load_image(path: list[str]) -> str:
         with open(file_path, 'rb') as file:
             return base64.b64decode(file.read()).decode('utf-8')
 
-def _generate_html(query, project: str, instance: str, database: str, mock: bool):
-        # Get the directory of the current file (magics.py)
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-
-        # Go up directories until we find the 'templates' folder
-        search_dir = current_dir
-        while 'frontend' not in os.listdir(search_dir):
-            parent = os.path.dirname(search_dir)
-            if parent == search_dir:  # We've reached the root directory after I updated
-                raise FileNotFoundError("Could not find 'frontend' directory")
-            search_dir = parent
-
-        # Retrieve the javascript content
-        template_content = _load_file([search_dir, 'frontend', 'static', 'index.html'])
-        schema_content = _load_file([search_dir, 'frontend', 'src', 'models', 'schema.js'])
-        graph_object_content = _load_file([search_dir, 'frontend', 'src', 'models', 'graph-object.js'])
-        node_content = _load_file([search_dir, 'frontend', 'src', 'models', 'node.js'])
-        edge_content = _load_file([search_dir, 'frontend', 'src', 'models', 'edge.js'])
-        config_content = _load_file([search_dir, 'frontend', 'src', 'spanner-config.js'])
-        store_content = _load_file([search_dir, 'frontend', 'src', 'spanner-store.js'])
-        menu_content = _load_file([search_dir, 'frontend', 'src', 'visualization', 'spanner-menu.js'])
-        graph_content = _load_file([search_dir, 'frontend', 'src', 'visualization', 'spanner-forcegraph.js'])
-        sidebar_content = _load_file([search_dir, 'frontend', 'src', 'visualization', 'spanner-sidebar.js'])
-        table_content = _load_file([search_dir, 'frontend', 'src', 'visualization', 'spanner-table.js'])
-        server_content = _load_file([search_dir, 'frontend', 'src', 'graph-server.js'])
-        app_content = _load_file([search_dir, 'frontend', 'src', 'app.js'])
-
-        # Retrieve image content
-        graph_background_image = _load_image([search_dir, "frontend", "static", "graph-bg.svg"])
-
-        # Create a Jinja2 template
-        template = Template(template_content)
-
-        # Render the template with the graph data and JavaScript content
-        html_content = template.render(
-            graph_background_image=graph_background_image,
-            template_content=template_content,
-            schema_content=schema_content,
-            graph_object_content=graph_object_content,
-            node_content=node_content,
-            edge_content=edge_content,
-            config_content=config_content,
-            menu_content=menu_content,
-            graph_content=graph_content,
-            store_content=store_content,
-            sidebar_content=sidebar_content,
-            table_content=table_content,
-            server_content=server_content,
-            app_content=app_content,
-            query=query,
-            port=GraphServer.port,
-            params=json.dumps({
-                 'project':project,
-                 'instance':instance,
-                 'database':database,
-                 'mock':mock
-            }),
-            id=uuid.uuid4().hex # Prevent html/js selector collisions between cells
-        )
-
-        return html_content
-
-
 def _parse_element_display(element_rep: str) -> dict[str, str]:
     """Helper function to parse element display fields into a dict."""
     if not element_rep:
@@ -171,7 +108,7 @@ class NetworkVisualizationMagics(Magics):
         # Generate the HTML content
         html_content = generate_visualization_html(
             query=self.cell,
-            url=GraphServer.url,
+            port=GraphServer.port,
             params={
                  'project':self.args.project,
                  'instance':self.args.instance,
